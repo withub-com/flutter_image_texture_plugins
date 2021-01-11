0087cc8a3fd0e85e173275328f91a7244ddda527
jiangyong <jiang@hy12371.com>
2021-01-11 22:25:28 +0800

    fix bug

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:flutterimagetexture/flutterimagetexture.dart';
+import 'package:flutterimagetexture/flutter_image_texture.dart';
 import 'package:flutterimagetexture/flutter_image_texture_widget.dart';
 
 void main() {
